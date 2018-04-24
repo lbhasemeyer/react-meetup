9b723f35e68b0762685a0c5358859a4dfafd999d
lkbhas <lbhasemeyer@gmail.com>
2018-04-23 22:07:01 -0600

    rough validations add

--- src/components/Slack.js
+++ src/components/Slack.js
@@ -4,22 +4,49 @@ import SlackLogo from '../slackLogo.svg';
 class Slack extends Component {
 	constructor(props) {
     super(props);
+    this._validateEmail = this._validateEmail.bind(this);
     this._changeEmail = this._changeEmail.bind(this);
     this._changeFirstLastName = this._changeFirstLastName.bind(this);
     this._signupClicked = this._signupClicked.bind(this);
     this._resetButtonText = this._resetButtonText.bind(this);
-    this.state = {email: null, firstLastName: null, buttonText: 'Request Invite'};
+    this.state = {email: null, firstLastName: null, emailValid: false, nameValid: false, buttonText: 'Request Invite'};
+  }
+	_validateEmail(email) {
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
   }
   _changeEmail(event) {
+  	if(this._validateEmail(event.target.value)){
+	  	this.setState({emailValid: true});
+  	}
   	this.setState({email: event.target.value});
   }
   _changeFirstLastName(event) {
+  	if(event.target.value.length > 0){
+	  	this.setState({nameValid: true});
+  	}
   	this.setState({firstLastName: event.target.value});
   }
    _signupClicked() {
-   	//THIS IS WHERE THE EMAIL AND NAME COME
-  	console.log(this.state);
-  	this.setState({buttonText: "Thanks! Please allow 24 hours for invite"});
+			document.getElementById("emailInput").style.border = null;
+			document.getElementById("nameInput").style.border = null;
+   	if(!this.state.emailValid){
+			document.getElementById("emailInput").style.border = '2px solid #8a83c2';
+   	} if(!this.state.firstLastName){
+			document.getElementById("nameInput").style.border = '2px solid #8a83c2';
+   	} else {
+	   	//THIS IS WHERE THE EMAIL AND NAME COME
+   		console.log(this.state.email);
+	  	console.log(this.state.firstLastName);
+	  	this.setState({buttonText: "Thanks! Please allow 24 hours for invite"});
+	  	this.setState({firstLastName: null});
+	  	this.setState({email: null});
+	  	this.setState({emailValid: false});
+	  	this.setState({nameValid: false});
+	  	//clear inputs
+	  	document.getElementById("emailInput").value = "";
+	  	document.getElementById("nameInput").value = "";
+   	}
   }
   _resetButtonText() {
   	if(this.state.buttonText !== "Request Invite"){
@@ -27,6 +54,10 @@ class Slack extends Component {
 	  }
   }
   render() {
+  	let emailValidated = (this.state.emailValid);
+  	let nameValidated = (this.state.nameValid);
+  	let buttonDisabled = (!this.state.emailValid || !this.state.nameValid)
+  	let buttonWhite = !buttonDisabled || this.state.buttonText === 'Thanks! Please allow 24 hours for invite';
     return (
     	<div className="App-signup">
 			<div className="App-signup-text">
@@ -35,9 +66,9 @@ class Slack extends Component {
 				<div style={{display: 'inline-block'}}>channel</div>
 			</div>
 			<div className="App-signup-inputs-and-button">
-				<input type="text" name="email" placeholder="Email" onClick={this._resetButtonText} onChange={this._changeEmail} />
-				<input type="text" name="firstlastname" placeholder="First and Last Name" onClick={this._resetButtonText} onChange={this._changeFirstLastName} />
-				<button onClick={this._signupClicked}>{this.state.buttonText}</button>
+				<input type="email" id="emailInput" name="email" placeholder="Email" onClick={this._resetButtonText} onChange={this._changeEmail} />
+				<input type="text" id="nameInput" name="firstlastname" placeholder="First and Last Name" onClick={this._resetButtonText} onChange={this._changeFirstLastName} />
+				<button onClick={this._signupClicked} style={{backgroundColor: buttonWhite ? 'white' : 'lightGray'}}>{this.state.buttonText}</button>
 			</div>
 		</div>
     );
